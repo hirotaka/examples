c4a5e20db525922213e538c3b82b9295bb95f477
Hirotaka Mizutani <52546+hirotaka@users.noreply.github.com>
2021-12-12 11:51:48 +0900

    First commit

---
+++ supabase-vue-auth/src/App.vue
@@ -0,0 +1,10 @@
+<script setup>
+import TheHeader from "@/components/Header.vue";
+</script>
+
+<template>
+  <the-header />
+  <div class="max-w-7xl p-10 m-auto">
+    <router-view />
+  </div>
+</template>
---
+++ supabase-vue-auth/src/components/Header.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="flex justify-between">
+    <ul class="flex gap-5 p-5">
+      <li><router-link :to="{ name: 'Home' }">ホーム</router-link></li>
+    </ul>
+  </div>
+</template>
