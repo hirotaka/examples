be6f2e4a47a4bef872b606901f767432ccfe1c19
Hirotaka Mizutani <52546+hirotaka@users.noreply.github.com>
2022-02-02 17:08:42 +0900

    Add initial clicks

--- supabase-nextjs-database/components/counter.tsx
+++ supabase-nextjs-database/components/counter.tsx
@@ -1,8 +1,32 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
+import { useUser } from '@/contexts/user'
+import { supabase } from '@/lib/supabase-client'
 
 export default function Counter() {
   const [clicked, setClicked] = useState(0)
   const [loading, setLoading] = useState()
+  const { user } = useUser()
+
+  useEffect(() => {
+    const getIntitialClicks = async () => {
+      try {
+        setLoading(true)
+        const { data } = await supabase
+          .from('profiles')
+          .select('clicks')
+          .eq('id', user.id)
+          .single()
+
+        setClicked(data.clicks)
+      } catch (error) {
+        alert(error.error_description || error.message)
+      } finally {
+        setLoading(false)
+      }
+    }
+
+    if (user) getIntitialClicks()
+  }, [user])
 
   const incrementCount = async () => {
     setClicked(clicked + 1)
