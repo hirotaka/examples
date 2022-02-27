fb173d938358d0ce2be27a4a6e86df9260e6bcb0
Hirotaka Mizutani <52546+hirotaka@users.noreply.github.com>
2022-02-27 19:14:54 +0900

    Add counter

--- supabase-nextjs-database/components/dashboard.tsx
+++ supabase-nextjs-database/components/dashboard.tsx
@@ -1,15 +1,17 @@
 import { useUser } from '@/contexts/user'
 import Message from '@/components/message'
 import Profile from '@/components/profile'
+import Counter from '@/components/counter'
 
 export default function Dashboard() {
-  const { logout } = useUser()
+  const { logout, user } = useUser()
 
   return (
     <div className="max-w-md mx-auto">
       <h1 className="text-2xl">ダッシュボード</h1>
       <Message />
       <Profile />
+      {user.profile && <Counter />}
       <div className="mt-6">
         <button className="secondary" onClick={() => logout()}>
           ログアウト
