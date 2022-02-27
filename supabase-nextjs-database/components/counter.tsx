4644767a1f6b8d330ce8520196b6bcbfda931700
Hirotaka Mizutani <52546+hirotaka@users.noreply.github.com>
2022-02-27 19:25:41 +0900

    Add function increment clicks

--- supabase-nextjs-database/components/counter.tsx
+++ supabase-nextjs-database/components/counter.tsx
@@ -12,15 +12,25 @@ export default function Counter() {
   }, [user])
 
   const incrementCount = async () => {
-    setClicked(clicked + 1)
+    try {
+      setLoading(true)
+      const { data } = await supabase.rpc('increment_clicks')
+      setClicked(data)
+    } finally {
+      setLoading(false)
+    }
   }
 
   return (
     <div className="mt-6">
       <label className="block">カウンター</label>
       <p className="mt-2">{loading ? '読み込み中...' : `${clicked}回`}</p>
-      <button className="primary mt-4" onClick={incrementCount}>
-        クリック
+      <button
+        className="primary mt-4"
+        onClick={incrementCount}
+        disabled={loading}
+      >
+        {loading ? '読み込み中...' : 'クリック'}
       </button>
     </div>
   )
