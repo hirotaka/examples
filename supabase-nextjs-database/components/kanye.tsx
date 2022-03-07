2ed27b44a6c9fe080416084dc0dd978a7cb8eb12
Hirotaka Mizutani <52546+hirotaka@users.noreply.github.com>
2022-03-07 16:15:17 +0900

    Add talk_to_kayne function

---
+++ supabase-nextjs-database/components/kanye.tsx
@@ -0,0 +1,33 @@
+import { useState, useEffect } from 'react'
+import { useUser } from '@/contexts/user'
+import { supabase } from '@/lib/supabase-client'
+
+export default function Kanye() {
+  const { user } = useUser()
+  const [loading, setLoading] = useState(false)
+  const [quote, setQuote] = useState()
+
+  useEffect(() => {
+    const rpcTalkToKanye = async () => {
+      try {
+        setLoading(true)
+        const { data } = await supabase.rpc('talk_to_kanye', {
+          name: user.profile.name,
+        })
+        setQuote(data)
+      } catch (error) {
+        alert(error.message)
+      } finally {
+        setLoading(false)
+      }
+    }
+    if (user) rpcTalkToKanye()
+  }, [user])
+
+  return (
+    <div className="mt-6">
+      <label className="block">Kanye</label>
+      <p className="mt-2">{loading ? '読み込み中...' : quote}</p>
+    </div>
+  )
+}
